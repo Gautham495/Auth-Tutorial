0968df229930c71d95f9780dab85055e3cc29e1a
Gautham495 <kinggautham495@gmail.com>
2022-01-19 08:17:45 +0530

    email auth

--- App.js
+++ App.js
@@ -1,26 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
-import auth from '@react-native-firebase/auth';
 import {
-  GoogleSignin,
-  GoogleSigninButton,
-} from '@react-native-google-signin/google-signin';
-
-GoogleSignin.configure({
-  webClientId:
-    '745305328860-3e60bmsvapbft8kh1dr0vf0u3h2239cp.apps.googleusercontent.com',
-});
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
-  const onGoogleButtonPress = async () => {
-    const {idToken} = await GoogleSignin.signIn();
-
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-    return auth().signInWithCredential(googleCredential);
-  };
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const onAuthStateChanged = async userAuth => {
     if (!userAuth) {
@@ -40,6 +33,36 @@ const App = () => {
     };
   }, []);
 
+  const signUp = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        alert('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          alert('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          alert('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+
+  const signIn = () => {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        alert('User Signed In');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   const signOut = async () => {
     auth().signOut();
 
@@ -48,27 +71,68 @@ const App = () => {
     return () => userReference();
   };
 
- 
-
   return (
     <SafeAreaView style={{alignItems: 'center', flex: 1, marginTop: 100}}>
       <View style={{margin: 10}}>
-        <Text>Google Sign In Tutorial</Text>
+        <Text>Email Sign In Tutorial</Text>
       </View>
 
       <View style={{margin: 10}}>
         {user === null && (
-          <GoogleSigninButton
-            style={{width: 312, height: 48}}
-            size={GoogleSigninButton.Size.Wide}
-            color={GoogleSigninButton.Color.Light}
-            onPress={onGoogleButtonPress}
-          />
+          <>
+            <TextInput
+              value={email}
+              onChangeText={e => setEmail(e)}
+              placeholder="Email"
+              style={{
+                borderWidth: 1,
+                margin: 10,
+                padding: 10,
+              }}></TextInput>
+            <TextInput
+              value={password}
+              onChangeText={e => setPassword(e)}
+              placeholder="password"
+              style={{borderWidth: 1, margin: 10, padding: 10}}></TextInput>
+            {isSignUp ? (
+              <>
+                <TouchableOpacity
+                  style={{
+                    borderWidth: 1,
+                    margin: 10,
+                    padding: 10,
+                    alignItems: 'center',
+                  }}
+                  onPress={() => signUp()}>
+                  <Text>Sign up</Text>
+                </TouchableOpacity>
+                <Text onPress={() => setIsSignUp(false)}>
+                  Already have an account? Sign In
+                </Text>
+              </>
+            ) : (
+              <>
+                <TouchableOpacity
+                  style={{
+                    borderWidth: 1,
+                    margin: 10,
+                    padding: 10,
+                    alignItems: 'center',
+                  }}
+                  onPress={() => signIn()}>
+                  <Text>Sign in</Text>
+                </TouchableOpacity>
+                <Text onPress={() => setIsSignUp(true)}>
+                  Already have an account? Sign In
+                </Text>
+              </>
+            )}
+          </>
         )}
       </View>
       {user !== null && (
         <View style={{margin: 10}}>
-          <Text style={{margin: 10}}>{user.displayName}</Text>
+          <Text style={{margin: 10}}>{user.email}</Text>
           <TouchableOpacity onPress={signOut} style={{alignItems: 'center'}}>
             <Text>Sign Out</Text>
           </TouchableOpacity>
